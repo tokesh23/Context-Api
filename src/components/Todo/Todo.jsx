08ec71f0e977b46d8e1caf5be8e47844ae174d33
tokesh23 <tokesh1998@gmail.com>
2024-06-06 10:51:59 +0530

    first commit

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todo = (todoData) => {
+  return (
+    <div>
+        <input type="checkbox" name="" id="" />
+        {todoData}
+        <Button>Edit</Button>
+        <Button>Delete</Button>
+    </div>
+  )
+}
+
+export default Todo
